f90e162575682e230f64776fbc3d2afa3aa13a93
Casey Lee <cplee@nektos.com>
2016-11-08 00:39:38 -0800

    base app with account list.  cognito is broken.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { AppComponent }         from './app.component';
+import { LoginComponent }       from './login.component';
+import { LogoutComponent }       from './logout.component';
+import { AccountListComponent } from './account-list.component';
+import { AppRoutingModule }     from './app-routing.module';
+import { MaterialModule }       from '@angular/material';
+import {AccountsService} from "./accounts.service";
 
-import { AppComponent } from './app.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MaterialModule.forRoot()
+    ],
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        LogoutComponent,
+        AccountListComponent,
+    ],
+    providers: [ AccountsService ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
